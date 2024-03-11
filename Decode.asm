735ca037f708b3df6491193b9d57b07c934945bc
4o4hasfound <138118407+4o4hasfound@users.noreply.github.com>
2024-03-11 23:05:21 +0800

    Update Decode.asm

--- Decode.asm
+++ Decode.asm
@@ -1,3 +1,10 @@
+; This is the solution to this zerojudge: https://zerojudge.tw/ShowProblem?problemid=a009
+;	在密碼學裡面有一種很簡單的加密方式，就是把明碼的每個字元加上某一個整數K而得到密碼的字元（明碼及密碼字元一定都在ASCII碼中可列印的範圍內）。
+;	例如若K=2，那麼apple經過加密後就變成crrng了。解密則是反過來做。這個問題是給你一個密碼字串，請你依照上述的解密方式輸出明碼。
+; 	(k == -7)
+
+
+
 .386
 .model flat, stdcall
 .stack 4096

8c4f44d314d772e403eae36dda631cba825a17c5
胡子新 <138118407+4o4hasfound@users.noreply.github.com>
2024-04-06 16:45:48 +0800

    Update BinarySearch.asm

--- BinarySearch.asm
+++ BinarySearch.asm
@@ -1,3 +1,7 @@
+; Input the number to be find
+; the program will then print "Target found in index: " following the found number's index
+; if the number is not found, the prorgam will print "Target not found"
+
 .386
 .model flat, stdcall
 .stack 4096
@@ -13,19 +17,19 @@ include irvine32.inc
 .code
 main PROC
 	call ReadInt
-	mov target, eax
+	mov target, eax ; Read the number into target
 
-	mov eax, 0
-	mov ebx, LENGTHOF list - 1
+	mov eax, 0 ; Left pointer
+	mov ebx, LENGTHOF list - 1 ; Right pointer
 	
-	cmp eax, ebx
+	cmp eax, ebx ; Leave the loop if lpointer = rpointer
 	je BSearchEnd
 	jmp BSearch
 	BSearch:
 		mov ecx, eax
 		add ecx, ebx
-		shr ecx, 1
-		mov edx, list[ecx * 4]
+		shr ecx, 1 ; ecx = eax + ebx >> 1
+		mov edx, list[ecx * 4] ; move the current number to compare into edx
 
 		cmp target, edx
 		
@@ -33,15 +37,15 @@ main PROC
 		jl Less
 		je Equal
 		
-		Greater:
-			mov eax, ecx
+		Greater: ; target is greater than the current number
+			mov eax, ecx ; move the ecx(middle pointer) to the left pointer
 			jmp NextLoop
-		Less:
+		Less: ; target is less than the current number
 			mov esi, ecx
 			sub esi, 1
-			mov ebx, esi
+			mov ebx, esi ; move the esi(middle pointer - 1) to the right pointer
 			jmp NextLoop
-		Equal:
+		Equal: ; target is found
 			mov edx, OFFSET found
 			call WriteString
 			mov eax, ecx
@@ -51,7 +55,7 @@ main PROC
 			cmp eax, ebx
 			je BSearchEnd
 			jmp BSearch
-	BSearchEnd:
+	BSearchEnd: ; target not found
 		mov edx, OFFSET notfound
 		call WriteString
 		call ExitProcess
